1180ec2d0ef9b8d53b326b4f008a0b1aa623335e
cichy380 <marcin.dobroszek@gmail.com>
2022-01-18 19:16:45 +0100

    Change layout/styles

--- apps/travelmaker-app/src/app/shared/material/material.module.ts
+++ apps/travelmaker-app/src/app/shared/material/material.module.ts
@@ -5,6 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatRippleModule } from '@angular/material/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -17,6 +18,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatIconModule,
     MatInputModule,
     MatListModule,
+    MatRippleModule,
     MatSnackBarModule,
     MatToolbarModule,
   ],
