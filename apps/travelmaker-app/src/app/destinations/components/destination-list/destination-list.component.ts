e8e6af9c231aaa37e8e1db75c8eba7b861c6bbbf
cichy380 <marcin.dobroszek@gmail.com>
2022-03-06 14:35:27 +0100

    Fix width of destination-list (#10)

--- apps/travelmaker-app/src/app/destinations/components/destination-list/destination-list.component.ts
+++ apps/travelmaker-app/src/app/destinations/components/destination-list/destination-list.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'travelmaker-destination-list',
   templateUrl: './destination-list.component.html',
+  styleUrls: ['./destination-list.component.scss']
 })
 export class DestinationListComponent {
 }
